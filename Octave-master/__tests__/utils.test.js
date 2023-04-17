9a4e69be55c4014ed86db7acac41be42ecf009b0
Liwen Tran <ltran29@jhu.edu>
2023-04-17 17:34:14 -0400

    Created tests to achieve full coverage of common.js

--- Octave-master/__tests__/utils.test.js
+++ Octave-master/__tests__/utils.test.js
@@ -1,7 +1,24 @@
 import * as common from "../src/utils/common";
 
 // Testing the utility functions in common.js
-describe('common', () => {
+// Using whitebox testing to achieve full statement coverage
+
+/*
+RESULTS of 
+> yarn test utils -t "common.js" --collectCoverage
+-----------|---------|----------|---------|---------|-------------------
+File       | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s 
+-----------|---------|----------|---------|---------|-------------------
+All files  |     100 |      100 |     100 |     100 |                   
+ common.js |     100 |      100 |     100 |     100 |                   
+-----------|---------|----------|---------|---------|-------------------
+Test Suites: 1 passed, 1 total
+Tests:       9 passed, 9 total
+Snapshots:   0 total
+Time:        0.398 s, estimated 1 s
+*/
+
+describe('common.js', () => {
     // achieving full SC for isValidUrl()
     describe('isValidUrl', () => {
         it('given a valid url', () => {
@@ -21,4 +38,56 @@ describe('common', () => {
             expect(welcome_text === 'Hey bob, continue listening to your Music' || welcome_text === "Hey bob, welcome back").toBeTruthy();
         });
     });
+
+    // achieving full SC for capitalize()
+    describe('capitalize', () => {
+        it('makes first letter uppercase', () => {
+            expect(common.capitalize('hello there')).toBe('Hello there');
+        });
+    });
+
+    // achieving full SC for createNamesArray()
+    describe('createNameArray', () => {
+        it('makes array', () => {
+            expect(common.createNamesArray('hello!')).toStrictEqual(['h', 'he','hel', 'hell','hello', 'hello!',]);
+        });
+    });
+
+    // achieving full SC for capitalizeAllWords()
+    describe('capitalizeAllWords', () => {
+        it('makes first letter of each word', () => {
+            expect(common.capitalizeAllWords('hello there')).toBe('Hello There');
+            expect(common.capitalizeAllWords('Hello there')).toBe('Hello There');
+        });
+    });
+
+    // achieving full SC for createNamesArrayWithCaptitalizedWords()
+    describe('createNamesArrayWithCaptitalizedWords', () => {
+        it('makes array', () => {
+            expect(common.createNamesArrayWithCaptitalizedWords('hello! hi')).toStrictEqual(['H', 'He','Hel', 'Hell', 'Hello', 'Hello!', 'Hello! ', 'Hello! H', 'Hello! Hi',]);
+        });
+    });
+
+    // achieving full SC for getRandomPlaylistImage()
+    describe('getRandomPlaylistImage', () => {
+        it('returns a valid image url', () => {
+            const url = common.getRandomPlaylistImage('bob');
+            expect(
+                url === 'https://firebasestorage.googleapis.com/v0/b/music-streaming-app-4a392.appspot.com/o/images%2Fno-music-cover.png?alt=media&token=c331e7e9-fbb6-41bd-8b63-706bd70429c7' ||
+                url === 'https://www.soundlaunch.com/cover_photos/default_cover.jpg' ||
+                url === 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/abstract-music-cd-cover-art-design-template-e43d2f90de58fd720f97f7e8068bb4f8_screen.jpg?ts=1570368591' ||
+                url === 'https://upload.wikimedia.org/wikipedia/en/c/c9/Zedd-True-Colors.png' ||
+                url === 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnJL-5T0rDIVexjW4ndJko_z_YnBtFmTRauQ&usqp=CAU'
+            ).toBeTruthy();          
+        });
+    });
+
+    // handle error
+    describe('handleError', () => {
+        it('prints to console', () => {
+            const consoleSpy = jest.spyOn(console, 'log');
+            common.handleError('Testing handleError is printing to logs');
+            expect(consoleSpy).toHaveBeenCalledWith('Testing handleError is printing to logs');
+        })
+    })
 });
