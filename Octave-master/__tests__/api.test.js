fe276d14e78c5011ecebe8ff09800a08c34367d3
Liwen Tran <ltran29@jhu.edu>
2023-04-18 15:03:47 -0400

    Attempting different ways to get firebase api testing to work

--- Octave-master/__tests__/api.test.js
+++ Octave-master/__tests__/api.test.js
@@ -1,12 +1,50 @@
-import { getArtists } from "../src/api/artist";
-
+import { getArtists, addArtist } from "../src/api/artist";
+import { handleError } from "../src/utils/common";
 // Testing the APIs related to Artist
-describe('Artist', () => {
-    test('get', () => {
-        return getArtists().then(data => {
-            expect(data).toBeDefined()
-            // expect(data.entity.name).toEqual('Koen van Gilst')
-        })
-    })
-});
+// describe('Artist', () => {
+//     it('123', async() => {
+//         const data = { description: "hey", name: "bob", names: ["bobs"], imageUrl: "sww.wogo.com"};
+//         await addArtist(data).catch(handleError);
+//     })
+//     it('get', async () => {
+//         const data = { description: "hey", name: "bob", names: ["bobs"], imageUrl: "sww.wogo.com"};
+//         const res2 =  await addArtist(data);
+//         console.log("addArtist res" + res2);
+//         const res = await getArtists((snapshot) => {
+//             const artists = [];
+//             snapshot.forEach((doc) => {
+//                 artists.push(doc.data().name);
+//             });
+//             console.log(artists);
+//         });
+//             // expect(data.entity.name).toEqual('Koen van Gilst')
+//         console.log('res' + res);
+//     });
+// });
 
+
+
+
+test('t1', async () => {
+    console.log('starting this test')
+    const res = await getArtists((snapshot) => {
+        
+        const artists = [];
+        snapshot.forEach((doc) => {
+            artists.push(doc.data().name);
+        });
+        console.log(artists);
+    });
+    console.log(res);
+},50000);
+
+// test('t2', async () => {
+//     const q = query(collection(db, "artists"), orderBy("name"));
+//     return onSnapshot(q, (snapshot) => {
+//         const artists = [];
+//         snapshot.forEach((doc) => {
+//             artists.push(doc.data().name);
+//             console.log(doc.data())
+//         });
+//     });
+// },50000);
--- Octave-master/jest.config.js
+++ Octave-master/jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    preset: 'ts-jest',
-    transform: {
-      '^.+\\.(ts|tsx)?$': 'ts-jest',
-      "^.+\\.(js|jsx)$": "babel-jest",
-    },
-    setupFilesAfterEnv: [
-      "<rootDir>/__tests__/mocks/localStorageMock.js"
-    ],
-    testEnvironment: 'jsdom'
-  };
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  setupFilesAfterEnv: [
+    "<rootDir>/__tests__/mocks/localStorageMock.js"
+  ],
+  testEnvironment: 'jsdom'
+};
