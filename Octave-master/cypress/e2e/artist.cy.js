680edd8bffd99a7e45e7723bfa18d00394e99f53
Dakh121 <77988764+Dakh121@users.noreply.github.com>
2023-04-21 11:16:54 -0400

    additional test

--- Octave-master/cypress/e2e/artist.cy.js
+++ Octave-master/cypress/e2e/artist.cy.js
@@ -51,13 +51,27 @@ describe('Testing Artist page', () => {
     })
   })
 
+  it('Test that adding song to queue from artist page functions properly', () =>{
+    cy.get('#root > div > div > div.app__window > div > div:nth-child(3) > div.row__songsContainer').contains('Justin Bieber').click()
+    cy.get('#root > div > div > div.app__window > div > div.artistpage__songlist > div:nth-child(1) > div.playlistsong__options > button').select(1)
+
+    cy.get('audio,video').should((els)=>{
+      let audible = false
+      els.each((i, el)=>{
+        console.log(el)
+        console.log(el.duration, el.paused, el.muted)
+        if (el.duration > 0 && !el.paused && !el.muted) {
+          audible = true
+        }
+  
+        // expect(el.duration > 0 && !el.paused && !el.muted).to.eq(false)
+      })
+      expect(audible).to.eq(true)
+    })
+  })
+
 
   afterEach(() => {
     cy.logout()
   })
-
-  // after(() => {
-  //   cy.logout()
-  // })
-
 })
