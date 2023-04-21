efcc7ef375c47f9846c2bbd48a1f9c55abdb042a
Dakh121 <77988764+Dakh121@users.noreply.github.com>
2023-04-20 20:15:40 -0400

    Search bugfixes

--- Octave-master/cypress/e2e/search.cy.js
+++ Octave-master/cypress/e2e/search.cy.js
@@ -3,19 +3,21 @@ describe('Testing Search Page', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
     cy.login()
-    cy.get('#root > div > div > div.navbar > div.navbar__center > a:nth-child(3) > span.navbar__linkSpan').click()
+    cy.get('#root > div > div > div.navbar > div.navbar__center > a:nth-child(3)').click()
   })
 
   //Test with no/null input
   it('Testing Search with No Input', () => {
-    cy.get('#root > div > div > div.app__window > div > div.search__bar > form > div > div > input').type()
+    cy.get('#root > div > div > div.app__window > div > div.search__bar > form > div > div > div > button').click()
+
+    cy.get("playlistsong").should('not.exist');
+    cy.get("artist").should('not.exist');
 
   })
 
-
   //Test with nonexistent values
   it('Testing Search with Value that Does not Exist', () => {
-    cy.get('#root > div > div > div.app__window > div > div.search__bar > form > div > div > input').type("Song123")
+    cy.get('#root > div > div > div.app__window > div > div.search__bar > form > div > div').type("Song123")
     cy.get('#root > div > div > div.app__window > div > div.search__bar > form > div > div > div > button').click()
     cy.contains("Search Result for Song123")
     cy.contains("No Results")
@@ -28,7 +30,6 @@ describe('Testing Search Page', () => {
     cy.contains("Search Result for Yum")
     cy.contains("Songs")
     cy.contains("Justin Bieber")
-    cy.contains("playlistsong")
   })
 
   //Test with artists that exist
@@ -38,11 +39,10 @@ describe('Testing Search Page', () => {
     cy.contains("Search Result for Bo")
     cy.contains("Artists")
     cy.contains("Bob")
-    cy.contains("artist")
   })
 
   //Test that second search clears items from first search
-  it('Testing com', () => {
+  it('Testing that another search clears first search', () => {
     cy.get('#root > div > div > div.app__window > div > div.search__bar > form > div > div > input').type("Baby")
     cy.get('#root > div > div > div.app__window > div > div.search__bar > form > div > div > div > button').click()
     cy.get('#root > div > div > div.app__window > div > div.search__bar > form > div > div > input').clear()
@@ -56,4 +56,9 @@ describe('Testing Search Page', () => {
     cy.contains("Artists")
     cy.contains("Shadow Rapper")
   })
+
+  afterEach(() => {
+    cy.logout()
+  })
+
 })
