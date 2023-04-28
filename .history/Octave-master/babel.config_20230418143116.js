1067bf90cf59eb0828e735f15f8afcd2fcfbf5d5
Liwen Tran <ltran29@jhu.edu>
2023-04-28 13:48:49 -0400

    Clean up uneccessary files

--- .history/Octave-master/babel.config_20230418143116.js
+++
@@ -1,7 +0,0 @@
-const { override, addBabelPlugins } = require("customize-cra")
-
-module.exports = override(
-  process.env.E2E_TESTS && addBabelPlugins(
-    "babel-plugin-istanbul",
-  )
-)
--- .history/Octave-master/cypress/plugins/index_20230418141544.js
+++
@@ -1,2 +0,0 @@
-module.exports = (on, config) => {
-    on('task', require('@cypress/code-coverage/task'))}
